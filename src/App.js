4e430e3af9ce0241e26f3c5917649a7a5775844a
Ifycode <marychord@gmail.com>
2022-03-16 16:39:19 +0100

    Minimal form component example

--- src/App.js
+++ src/App.js
@@ -1,9 +1,56 @@
+import React, { useState } from 'react';
+import { createContainer } from 'unstated-next';
 
-function App() {
+// useForm hook
+const useForm = () => {
+  const [input, setInputValue] = useState('');
+  const [name, setName] = useState('Ifycode');
+
+  const handleInput = event => {
+    setInputValue(event.target.value);
+  };
+
+  const updateName = event => {
+    event.preventDefault();
+    setName(input);
+    setInputValue('');
+  };
+
+  return {
+    input,
+    name,
+    handleInput,
+    updateName,
+  };
+};
+
+// Create a container for use across our application
+const FormContainer = createContainer(useForm);
+
+// Form component
+const Form = () => {
+  const form = FormContainer.useContainer();
   return (
     <div>
-      Unstated next tutorial!
+      <p>Hello! {form.name}</p>
+      <div>
+        <input
+          type="text"
+          value={form.input}
+          onChange={form.handleInput}
+        />
+        <button onClick={form.updateName}>Save</button>
+      </div>
     </div>
+  );
+};
+
+// App component
+function App() {
+  return (
+    <FormContainer.Provider>
+      <Form />
+   </FormContainer.Provider>
   );
 }
 
